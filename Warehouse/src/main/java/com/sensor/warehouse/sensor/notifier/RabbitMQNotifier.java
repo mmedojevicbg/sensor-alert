374387e3a2f09cdb26e65d630f3b0b125d0d3af9
Marko Medojevic <mmedojevicbg@gmail.com>
2025-01-06 10:45:33 +0100

    dockerization - environment variables

--- Warehouse/src/main/java/com/sensor/warehouse/sensor/notifier/RabbitMQNotifier.java
+++ Warehouse/src/main/java/com/sensor/warehouse/sensor/notifier/RabbitMQNotifier.java
@@ -24,11 +24,15 @@ public class RabbitMQNotifier implements Notifier {
     }
 
     private void initRabbitMQ() throws IOException, TimeoutException {
+        String rabbitmqUser = System.getenv("RABBITMQ_USER");
+        String rabbitmqPass = System.getenv("RABBITMQ_PASS");
+        String rabbitmqHost = System.getenv("RABBITMQ_HOST");
+        String rabbitmqPort = System.getenv("RABBITMQ_PORT");
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        factory.setUsername("guest");
-        factory.setPassword("guest");
-        factory.setPort(6672);
+        factory.setHost(rabbitmqHost != null ? rabbitmqHost : "localhost");
+        factory.setUsername(rabbitmqUser != null ? rabbitmqUser : "guest");
+        factory.setPassword(rabbitmqPass != null ? rabbitmqPass : "guest");
+        factory.setPort(rabbitmqPort != null ? Integer.parseInt(rabbitmqPort) : 6672);
         Connection connection = factory.newConnection();
         channel = connection.createChannel();
         channel.queueDeclare("sensor_queue", false, false, false, null);
